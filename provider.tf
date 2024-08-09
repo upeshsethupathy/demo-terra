529ddad784b41b53c5f634fb94d7d3474ef641ca
upesh <upeshsethupathy18@gmail.com>
2024-08-09 09:22:33 +0530

    commit

--- provider.tf
+++ provider.tf
@@ -1,3 +1,3 @@
-provider "aws"{
-    region = "us-east-1"
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0504227d6d64040667ee7b0fdba70bcbe0048c1806c6e75ca557cc4d525313e6
+size 44
--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
-variable "cidr" {
-    default = "10.0.0.0/16"
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:55f0316bf3199ed68f6d92d66bb40fec5f3bccc7e8ef7aa7688cd48bdfab14a7
+size 49
